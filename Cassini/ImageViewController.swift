8f806966e27e8a125debda4722d7cab3ef1428c6
Shakaib Akhtar <shakaib.developer@gmail.com>
2019-08-16 18:50:16 +0500

    ScrollsView Demo Created

---
+++ Cassini/ImageViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ImageViewController.swift
+//  Cassini
+//
+//  Created by Shakaib Akhtar on 16/08/2019.
+//  Copyright © 2019 iParagons. All rights reserved.
+//
+
+import UIKit
+
+class ImageViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    
+}
