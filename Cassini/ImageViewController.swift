e477d01c0ff4ba5a7f8d50f6f0b01b30dd256a1f
Shakaib Akhtar <shakaib.developer@gmail.com>
2019-08-16 19:22:14 +0500

    Add image to scroll view, TODO add scroll functionality

--- Cassini/ImageViewController.swift
+++ Cassini/ImageViewController.swift
@@ -9,7 +9,37 @@
 import UIKit
 
 class ImageViewController: UIViewController {
+    var imageUrl: URL? {
+        didSet {
+            imageView.image = nil
+            if view.window != nil {
+                fetchImage()
+            }
+        }
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        if imageView.image == nil {
+            fetchImage()
+        }
+    }
 
     @IBOutlet weak var imageView: UIImageView!
     
+    private func fetchImage() {
+        if let url = imageUrl {
+            let urlContents = try? Data(contentsOf: url)
+            if let imageData = urlContents {
+                imageView.image = UIImage(data: imageData)
+            }
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        if imageUrl == nil {
+            imageUrl = DemoURLs.stanford
+        }
+    }
 }
